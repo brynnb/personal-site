c6ebc8446866fabeadba7f2776d9f1d9943a280c
brynnb <brynnb@users.noreply.github.com>
2026-05-14 12:35:38 -0700

    Update capturequest wording

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -73,7 +73,7 @@ export default class DataService {
             },
             {
               title: "CaptureQuest",
-              description: "An expansive 2D multiplayer world built with the Phaser engine. Real-time world interaction, monster collection, and trainer battles, all running in the browser with a custom Go server backend. Also includes a complete data extraction pipeline that parses original Game Boy Pokémon ROM assembly to pull out maps, sprites, encounters, and game logic for use in the web client.",
+              description: "An online multiplayer version of Pokemon Red/Blue, recreated from the original game data on the GameBoy cartridge! A simple proof of concept demo with real-time world interaction, monster collection, and trainer battles, all running in the browser with a custom Go server backend. Also includes a complete data extraction pipeline that parses original Game Boy Pokémon ROM assembly to pull out maps, sprites, encounters, and game logic for use in the web client.",
               techStack: "React, Phaser, Go, WebTransport, MySQL",
               url: "https://capturequest.net",
               repo: "https://github.com/brynnb/capture-quest",
